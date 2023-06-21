c00b78499c126f538cc3365f4f99d76010fa67da
Ilya <ilya.tumanow2015@yandex.ru>
2023-06-21 23:16:43 +0300

    tasks 3 and 4 solved

--- Task001_lesson001/Program.cs
+++ Task001_lesson001/Program.cs
@@ -21,3 +21,21 @@ if (max < d) max = d;
 if (max < e) max = e;
 
 System.Console.WriteLine("Максимальное из этих чисел = " + max);
+
+//Задача 3
+
+Console.WriteLine("Введите число");
+int f = Convert.ToInt32(Console.ReadLine());
+if (f % 2 == 0)
+    System.Console.WriteLine("Число:" + f + " четное");
+else
+    System.Console.WriteLine($"Число: {f} нечетное ");
+
+//Задача 4
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    if (i % 2 == 0)
+        System.Console.Write(i + ", ");
+}
