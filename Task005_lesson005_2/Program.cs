eb7c7f31a9281be7e5df51f6eac79e344783cfd1
Ilya <ilya.tumanow2015@yandex.ru>
2023-07-01 15:05:44 +0300

    code formated

--- Task005_lesson005_2/Program.cs
+++ Task005_lesson005_2/Program.cs
@@ -6,24 +6,24 @@ int[] randomArray = new int[a];
 
 void mas(int a)
 {
-for (int i = 0; i < a; i++)
-{
-randomArray[i] = new Random().Next(1,9);
-Console.Write(randomArray[i] + " ");
-}
+    for (int i = 0; i < a; i++)
+    {
+        randomArray[i] = new Random().Next(1, 9);
+        Console.Write(randomArray[i] + " ");
+    }
 
 }
 
 int kol(int[] randomArray)
 {
-int sum = 0;
-int i = 0;
-while (i < randomArray.Length)
-{
-sum = sum + randomArray[i];
-i = i + 2;
-}
-return sum;
+    int sum = 0;
+    int i = 0;
+    while (i < randomArray.Length)
+    {
+        sum = sum + randomArray[i];
+        i = i + 2;
+    }
+    return sum;
 }
 
 mas(a);
