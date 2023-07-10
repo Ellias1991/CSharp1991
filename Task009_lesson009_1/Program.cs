566df5d65307f309bdd2be80f2a48fc01da3aa17
Ilya <ilya.tumanow2015@yandex.ru>
2023-07-10 23:29:03 +0300

    task1 solved

---
+++ Task009_lesson009_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Урок 9,задача 1
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void chet(int m, int n)
+{
+    if (m > n)
+        return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m}, ");
+    }
+    chet(m + 1, n);
+
+}
+chet(m, n);
